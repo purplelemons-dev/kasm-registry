66cef1b19d2871d340422265946bd05911b4e106
Josh Smith <63922192+purplelemons-dev@users.noreply.github.com>
2024-08-20 23:36:47 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Purplelemons test',
+    description: 'my first kasm repo',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://purplelemons-dev.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/purplelemons-dev#contact',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
